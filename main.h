f4036af44bc3e706e7409b177942892bdd1cba7f
chukky01 <85424159+chukky01@users.noreply.github.com>
2022-07-12 17:13:28 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
+
+
 #include <stdarg.h>
+
+
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
@@ -19,4 +23,17 @@ int print_base16_upper_lower(va_list arg, char *representation);
 int loop_format(va_list arg, const char *string);
 int call_function_manager(char aux, va_list arg);
 int check_percent(int *flag, char aux);
+
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
+
+
 #endif
